d09dd836239db3aa3762f1251d66cd3412dc5070
ChristianA (PC) <christiand.apostol@outlook.com>
2022-01-10 16:46:40 -0500

    Added ReadNote(), DeleteNote(), ShowNotes(), Exit(), NotesDirectory(), and CommandsAvailable() Methods

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Xml;
 using System.Xml.Linq;
 using System.Linq;
 using System.IO;
+using System.Diagnostics;
 
 // Tutorial Link: https://www.thecodingguys.net/blog/csharp-create-command-line-notes-application
 // XML Documentation: https://docs.microsoft.com/en-us/dotnet/api/system.xml.xmlwriter?view=net-6.0
@@ -53,7 +54,7 @@ namespace Note_Taking_App
                     NotesDirectory();
                     Main(null);
                     break;
-                case "cls":
+                case "close":
                     Console.Clear();
                     Main(null);
                     break;
@@ -70,8 +71,13 @@ namespace Note_Taking_App
         // Create a new note
         private static void NewNote()
         {
-            Console.WriteLine("Please Enter Note:\n");
-            string input = Console.ReadLine();
+            // Get Note Title
+            Console.WriteLine("Enter note title:\n");
+            string inputTitle = Console.ReadLine();
+
+            // Get Note Content
+            Console.WriteLine("Enter note content\n");
+            string inputBody = Console.ReadLine();
 
             XmlWriterSettings NoteSettings = new XmlWriterSettings();
 
@@ -79,14 +85,14 @@ namespace Note_Taking_App
             NoteSettings.ConformanceLevel = ConformanceLevel.Auto;
             NoteSettings.Indent = true;
 
-            string fileName = DateTime.Now.ToString("dd-MM-yy") + ".xml";
+            string fileName = inputTitle + ".xml";
 
             using (XmlWriter NewNote = XmlWriter.Create(NoteDirectory + fileName, NoteSettings))
             {
 
                 NewNote.WriteStartDocument();
                 NewNote.WriteStartElement("Note");
-                NewNote.WriteElementString("body", input);
+                NewNote.WriteElementString("body", inputBody);
                 NewNote.WriteEndElement();
 
                 NewNote.Flush();
@@ -98,7 +104,7 @@ namespace Note_Taking_App
         private static void EditNote()
         {
             Console.WriteLine("Enter File Name:\n");
-            string fileName = Console.ReadLine().ToLower();
+            string fileName = Console.ReadLine().ToLower() + ".xml";
 
             // Check to see if file exists
             if (File.Exists(NoteDirectory + fileName))
@@ -139,36 +145,140 @@ namespace Note_Taking_App
             }
         }
 
+        // Delete a note
+        private static void DeleteNote()
+        {
+            Console.WriteLine("Enter File Name:\n");
+            string fileName = Console.ReadLine().ToLower();
+
+            // Check to see if Note exists
+            if (File.Exists(NoteDirectory + fileName))
+            {
+                Console.WriteLine(Environment.NewLine + "Are you sure you wish to delete this file? Y/N\n");
+
+                string Confirmation = Console.ReadLine().ToLower();
+
+                if (Confirmation == "y")
+                {
+                    try
+                    {
+                        File.Delete(NoteDirectory + fileName);
+                        Console.WriteLine("File has been deleted\n");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("File not deleted following error occured: " + ex.Message);
+                    }
+                }
+
+                else if (Confirmation == "n")
+                {
+                    Main(null);
+                }
+
+                // If input is neither "y" or "n"
+                else
+                {
+                    Console.WriteLine("Invalid command\n");
+                    DeleteNote();
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("File does not exist\n");
+                DeleteNote();
+            }
+        }
+
+        // Open a note for user to view
+        private static void ReadNote()
+        {
+            Console.WriteLine("Enter File Name:\n");
+            string fileName = Console.ReadLine() + ".xml";
+
+            if (File.Exists(NoteDirectory + fileName))
+            {
+                XmlDocument doc = new XmlDocument();
+
+                try
+                {
+                    doc.Load(NoteDirectory + fileName);
+                    Console.Write(doc.SelectSingleNode("//body\n").InnerText);
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Could not read note, following message occured: " + ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("File not found\n");
+            }
+        }
+
+        // Print all possible commands
         private static void CommandsAvailable()
         {
-            throw new NotImplementedException();
-        }
+            string[] commandList = new string[8];
+            commandList[0] = "new - Create a new note";
+            commandList[1] = "edit - Edit a note";
+            commandList[2] = "read - Read a note";
+            commandList[3] = "delete - Delete a note";
+            commandList[4] = "shownotes - List all notes";
+            commandList[5] = "dir - Open Note Directory";
+            commandList[6] = "close - Clear Console";
+            commandList[7] = "exit - Exit Application";
 
-        private static void Exit()
-        {
-            throw new NotImplementedException();
-        }
-
-        private static void NotesDirectory()
-        {
-            throw new NotImplementedException();
+            for (int i = 0; i < commandList.Length; i++)
+            {
+                Console.WriteLine(commandList[i] + "\n");
+            }
         }
 
         private static void ShowNotes()
         {
-            throw new NotImplementedException();
+            string NoteLocation = NoteDirectory;
+
+            DirectoryInfo Dir = new DirectoryInfo(NoteLocation);
+
+            if (Directory.Exists(NoteLocation))
+            {
+                FileInfo[] NoteFiles = Dir.GetFiles("*.xml");
+
+                if (NoteFiles.Count() != 0)
+                {
+                    Console.SetCursorPosition(Console.CursorLeft + 1, Console.CursorTop + 2);
+                    Console.WriteLine("+------------+");
+                    foreach (var Note in NoteFiles)
+                    {
+                        Console.WriteLine("   " + Note.Name);
+                    }
+                    Console.WriteLine(Environment.NewLine);
+
+                }
+                else
+                {
+                    Console.WriteLine("No notes found.\n");
+                }
+            }
+            else
+            {
+                Console.WriteLine(" Directory does not exist.....creating directory\n");
+                Directory.CreateDirectory(NoteLocation);
+                Console.WriteLine(" Directory: " + NoteLocation + " created successfully.\n");
+            }
         }
 
-        private static void DeleteNote()
+        private static void Exit()
         {
-            throw new NotImplementedException();
+            Environment.Exit(0);
         }
 
-        private static void ReadNote()
+        private static void NotesDirectory()
         {
-            throw new NotImplementedException();
+            Process.Start("explorer.exe", NoteDirectory);
         }
-
-
     }
 }
