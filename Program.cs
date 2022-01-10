7f00c59a7e6afdbc81c73946ddf53f7b1cb7160b
ChristianA (PC) <christiand.apostol@outlook.com>
2022-01-10 14:43:59 -0500

    Adding Console App files

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+using System.Linq;
+
+namespace Note_Taking_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
